a57d9ced3cf134a6ffc7b67099b76ca4b7645128
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-02-08 16:29:04 -0300

    Adição de parâmetro nas anotações do cucumber

--- src/test/java/br/com/fiap/soat/bdd/RunCucumberTest.java
+++ src/test/java/br/com/fiap/soat/bdd/RunCucumberTest.java
@@ -1,5 +1,6 @@
 package br.com.fiap.soat.bdd;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
@@ -11,5 +12,6 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectPackages("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.com.fiap.soat.bdd")
 public class RunCucumberTest {
 }
