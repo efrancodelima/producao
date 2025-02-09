72411a9c97bdadbb36802a25053b11dc12863b8b
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-02-09 19:45:25 -0300

    Atualização do script de deploy

--- s3-ecr-push.sh
+++ s3-ecr-push.sh
@@ -1,15 +1,32 @@
-#!/bin/sh
-# Bourne-Again shell script, ASCII text executable
+#!/bin/bash
 
-# Pega a versão do projeto para uma variável de ambiente
-JAVA_HOME="/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/17.0.13-11/x64"
-FULL_PATH_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/app-producao"
-PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+# Inicia o script
+echo "Script iniciado."
 
-# Faz o upload da imagem docker para a AWS ECR com a tag da versão do projeto
-docker tag app-producao $FULL_PATH_IMAGE:$PROJECT_VERSION
-docker push $FULL_PATH_IMAGE:$PROJECT_VERSION
+CLUSTER_NAME="cluster-lanchonete"
+SERVICE_NAME="producao-service"
+TASK_DEF_NAME="task-def-producao"
 
-# Faz o upload da imagem docker para a AWS ECR com a tag latest
-docker tag app-producao $FULL_PATH_IMAGE:latest
-docker push $FULL_PATH_IMAGE:latest
+# Clona a task definition mais recente, removendo os campos desnecessários
+NEW_TASK_DEFINITION=$(aws ecs describe-task-definition \
+  --task-definition ${TASK_DEF_NAME} --output json | \
+  jq '.taskDefinition | del(.taskDefinitionArn, .revision, .status, .requiresAttributes, .compatibilities, .registeredAt, .registeredBy)')
+
+# Registra a nova task definition
+REGISTERED_TASK=$(aws ecs register-task-definition --cli-input-json \
+  "${NEW_TASK_DEFINITION}" --output json)
+
+echo "Registrou a nova task definition."
+
+# Pega o número da revisão da nova task definition
+NR_REV_NOVA=$(aws ecs describe-task-definition --task-definition ${TASK_DEF_NAME} \
+  --output json | jq -r '.taskDefinition.revision')
+
+# Atualiza o service
+UPDATE=$(aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} \
+  --task-definition ${TASK_DEF_NAME}:${NR_REV_NOVA})
+
+echo "Atualizou o service."
+
+# Encerra o script
+echo "Script finalizado."
