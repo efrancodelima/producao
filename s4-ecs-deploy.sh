86e14fbed94ca04d6c2ca18f6a5116a0d0bfe38f
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-02-09 19:51:09 -0300

    Correção dos scripts (troquei o conteúdo deles)

--- s4-ecs-deploy.sh
+++ s4-ecs-deploy.sh
@@ -3,22 +3,30 @@
 # Inicia o script
 echo "Script iniciado."
 
-# Clona a task definition atual, removendo os campos desnecessários
-TASK_DEF_NAME="lanchonete-task-definition"
-NEW_TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition ${TASK_DEF_NAME} --output json 2>/dev/null | \
-  jq '.taskDefinition' | \
-  jq 'del(.taskDefinitionArn, .revision, .status, .requiresAttributes, .compatibilities, .registeredAt, .registeredBy)')
+CLUSTER_NAME="cluster-lanchonete"
+SERVICE_NAME="producao-service"
+TASK_DEF_NAME="task-def-producao"
+
+# Clona a task definition mais recente, removendo os campos desnecessários
+NEW_TASK_DEFINITION=$(aws ecs describe-task-definition \
+  --task-definition ${TASK_DEF_NAME} --output json | \
+  jq '.taskDefinition | del(.taskDefinitionArn, .revision, .status, .requiresAttributes, .compatibilities, .registeredAt, .registeredBy)')
 
 # Registra a nova task definition
-REGISTERED_TASK=$(aws ecs register-task-definition --cli-input-json "${NEW_TASK_DEFINITION}" --output json 2>/dev/null)
+REGISTERED_TASK=$(aws ecs register-task-definition --cli-input-json \
+  "${NEW_TASK_DEFINITION}" --output json)
 
-# Atualiza o serviço para usar a nova task definition
-CLUSTER_NAME="lanchonete-ecs-cluster"
-SERVICE_NAME="lanchonete-ecs-service"
-NEW_TASK_REVISION=$(echo $REGISTERED_TASK | jq -r '.taskDefinition.revision')
+echo "Registrou a nova task definition."
 
-UPDATE=$(aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_DEF_NAME}:$NEW_TASK_REVISION --output json 2>/dev/null)
+# Pega o número da revisão da nova task definition
+NR_REV_NOVA=$(aws ecs describe-task-definition --task-definition ${TASK_DEF_NAME} \
+  --output json | jq -r '.taskDefinition.revision')
+
+# Atualiza o service
+UPDATE=$(aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} \
+  --task-definition ${TASK_DEF_NAME}:${NR_REV_NOVA})
+
+echo "Atualizou o service."
 
 # Encerra o script
-echo "Deploy realizado com sucesso!"
 echo "Script finalizado."
