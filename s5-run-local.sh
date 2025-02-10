630802f39a540e5f8cbd223bb9779d06684913a0
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-02-09 21:11:36 -0300

    Ajuste de variável no script

--- s5-run-local.sh
+++ s5-run-local.sh
@@ -2,6 +2,6 @@
 
 export H2_USERNAME=sa
 export H2_PASSWORD=password
-export URL_SERV_PAGAMENTO=empty
+export URL_SERV_PAGAMENTO="http://localhost:8081/"
 
 mvn spring-boot:run -Dspring-boot.run.profiles=test
